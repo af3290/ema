7665e51a96c123dc49dba13862c423d774d9fc8a
af3290 <firte.andrei@yahoo.com>
2016-02-05 14:05:43 +0100

    added spike estimation based on moving averages/standard deviation

--- MarketModels/Preprocess.fs
+++ MarketModels/Preprocess.fs
@@ -3,6 +3,11 @@
 module Preprocess =
     open System
     open System.Collections.Generic
+    open Types
+    open Forecast
+    open TimeSeries
+    open MathFunctions
+    open Operations
     open MathNet.Numerics
 
     //clearning and preprocessing data here...
@@ -35,3 +40,34 @@ module Preprocess =
             if x > ub then series.[i, j] <- ub 
             if x < lb then series.[i, j] <- lb 
         )
+
+    let Desezonalize (series : float[]) (lag : int) : float[] =
+        //Simple moving average case
+        let b = Array.init lag (fun i -> 1.0 / (float)lag)        
+        
+        let movavg = MovAvg series lag
+
+        (series -- movavg) .+. mean series
+
+    ///Returns the indices where spikes are determined based on moving standard deviation at given lags and confidence level
+    let EstimateSpikesOnMovSDs (series : float[]) (longLag : int) (shortLag : int) (alpha : float) : int[] = 
+        
+        let shortSDs = MovingStandardDeviation series MovingAverageType.Simple shortLag 1
+
+        let longSDs = MovingStandardDeviation series MovingAverageType.Simple longLag 1
+
+        let isSpikeIndices = Array.init series.Length (fun i -> if shortSDs.[i] > alpha * longSDs.[i] then i - shortLag/2 else -1) 
+                
+        isSpikeIndices |> Array.filter (fun x -> x > 0)
+
+    ///IN TODO...
+    let ReplaceSpikes (series : float[]) (spikeIndices : int[]) (sp : SpikePreprocess) (alpha : float) : float[] = 
+        //find unique ocurring spike
+        let singularSpikes = Array.init (spikeIndices.Length - 2) (fun i -> if spikeIndices.[i] < spikeIndices.[i+1] && spikeIndices.[i+1] < spikeIndices.[i+2] then i else -1)
+        
+        //also need to find spike cluters and the appropriate period's replacement
+        let spikeCluters = Array.init (spikeIndices.Length - 2) (fun i -> if spikeIndices.[i] < spikeIndices.[i+1] && spikeIndices.[i+1] < spikeIndices.[i+2] then i else -1)
+
+        //etc...
+
+        series
