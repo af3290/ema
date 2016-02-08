bc3b3aff7b7e840fbd7eb8ce3358c0b426d86d72
af3290 <firte.andrei@yahoo.com>
2016-02-08 13:32:05 +0100

    some extension methods;

--- Utils/Extensions.cs
+++ Utils/Extensions.cs
@@ -18,5 +18,46 @@ namespace Utils
             }
             return x;
         }
+
+        /// <summary>
+        /// Interprets the lists as columns lists and makes a corresponding matrix
+        /// </summary>
+        /// <param name="lists"></param>
+        /// <returns></returns>
+        public static double[,] ColumnsToRectangularArray(this List<List<double>> lists)
+        {
+            if (lists.Count == 0)
+                return new double[0, 0];
+            
+            var exData = new double[lists[0].Count, lists.Count];
+
+            for (int i = 0; i < lists.Count; i++)
+            {
+                for (int j = 0; j < lists[i].Count; j++)
+                {
+                    exData[j, i] = lists[i][j];
+                }
+            }
+
+            return exData;
+        }
+
+        public static bool HasInvalidData(this double[] vec)
+        {
+            return vec.Any(x => double.IsNaN(x) || double.IsInfinity(x));
+        }
+
+        public static bool HasInvalidData(this double[,] mat)
+        {
+            //for (int i = 0; i < mat.Count; i++)
+            //{
+            //    for (int j = 0; j < lists[i].Count; j++)
+            //    {
+            //        exData[j, i] = lists[i][j];
+            //    }
+            //}
+            var flattened = mat.Cast<double>().ToArray();
+            return flattened.HasInvalidData();
+        }
     }
 }
