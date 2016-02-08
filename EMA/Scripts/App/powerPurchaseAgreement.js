82e2ae45506ddf44e522bec33daf15ec663136f4
af3290 <firte.andrei@yahoo.com>
2016-02-08 13:36:37 +0100

    merged spot simulation scrips...etc..

---
+++ EMA/Scripts/App/powerPurchaseAgreement.js
@@ -0,0 +1,134 @@
+﻿emaDemos.controller('ForecastController', function ($scope, $http, blockUI) {
+    $scope.$http = $http;
+
+    $scope.postUrl = {
+        "Data": "/Prices/HistoricalSystemPrice?refresh=false&resolution=Daily",
+        "Valuation": "/Valuations/PowerPurchaseAgreement"
+    };
+
+    $scope.onChangedPostResponse = { "Data": dataSuccess, "Valuation": valuationSuccess };
+
+    $scope.currentFrameId = "Valuation"; //all changes are posted there...
+    //prepare variables... put somewhere...
+    $scope.httpPostParameters = [];
+    $scope.objectWatchers = {};
+
+    registerParameter($scope, 'retailPrice', 30);
+    registerParameter($scope, 'horizon', 3);
+    registerParameter($scope, 'capacity', 100);
+    registerParameter($scope, 'margin', 5);
+    registerParameter($scope, 'confidence', 0.995);
+    
+    $scope.Currency = "€";
+
+    var chart = $('#container').highcharts('StockChart', {
+        chart: {
+            zoomType: 'xy'
+        },
+        tooltip: {
+            valueDecimals: 4
+        },
+        rangeSelector: {
+            selected: 1
+        },
+        title: {
+            text: 'Simulations'
+        },
+        xAxis: {
+            ordinal: false,
+            type: 'datetime'
+            //,tickInterval: 86400000
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            borderWidth: 0
+        },
+        series: []
+    });
+
+    window.chart = chart = $('#container').highcharts();
+    
+    /* Private methods */
+    function prcFrmt(prc) {
+        return (prc*100).toFixed(2);
+    }
+
+    var startingDate, startingDateHistorical;
+
+    function valuationSuccess(dataObj) {
+        clearSeriesContainingName(chart, "Simulated");
+
+        var histChart = createHistogram();
+        plotDataToHistogram(histChart, dataObj.Histogram);
+
+        /* Do forecast */
+        var simulations = dataObj.Simulations;
+        
+        for (var i = 0; i < simulations.length; i++) {
+            var simulation = simulations[i];
+            var simulationData = [];
+            for (var j = 0; j < simulation.length; j++) {
+                simulationData[j] = [startingDate + j * TICKS_IN_DAY, simulation[j]];
+            }
+
+            var serie = {
+                name: "Simulated Path " + i,
+                color: "#FF0000",
+                dashStyle: "ShortDot",
+                data: simulationData
+            };
+
+            chart.addSeries(serie);
+        }
+        
+        
+        //todo: fix UTC...
+        var startDate = new Date(startingDateHistorical);
+        //Adjust for utc and include eventual backcasting
+        startDate.setTime(startDate.getTime() + (2 * 60 * 60 * 1000));
+        var rangeStartDate = Date.parse(startDate);
+
+        var endDate = new Date(startingDate);
+        //Must show only until hour 23
+        endDate.setTime(endDate.getTime() + (1 * 60 * 60 * 1000) + $scope.horizon * 365 * TICKS_IN_DAY);
+        var rangeEndDate = Date.parse(endDate);
+
+        chart.xAxis[0].setExtremes(rangeStartDate, rangeEndDate);
+
+        $scope.extendWith('Results', dataObj);
+        $scope.extendWithAndListen('MathModel', dataObj);
+
+        blockUI.stop();
+    }
+    
+    function dataSuccess(data) {
+        startingDateHistorical = Date.parse(data[0].DateTime);
+        startingDate = Date.parse(data[data.length - 1].DateTime);
+
+        var priceData = [];
+        for (var i = 0; i < data.length; i++) {
+            //try using linq.js => YES!
+            var dt = Date.parse(data[i].DateTime);
+            priceData[i] = [dt, data[i].Value];
+        }
+
+        var serie = {
+            name: "Historical Price",
+            color: '#CFCFCF',
+            data: priceData,
+            tooltip: {
+                valueDecimals: 4
+            },
+            zIndex: 9
+        };
+
+        chart.addSeries(serie);
+        
+        doPageParametersPost($scope, "Valuation");
+    }
+    
+    /* Start all */
+    doPageParametersPost($scope, "Data");
+});
--- EMA/Scripts/electricitySpotSimulations.js
+++ EMA/Scripts/electricitySpotSimulations.js
@@ -5,6 +5,64 @@
     $scope.simulationsCount = 0;
     $scope.simulationSeries = [];
 
+    var chart = $('#container').highcharts('StockChart', {
+        tooltip: {
+            valueDecimals: 4
+        },
+        rangeSelector: {
+            selected: 1
+        },
+        title: {
+            text: 'Nordic Forward Contracts... :D'
+        },
+        xAxis: {
+            ordinal: false,
+            type: 'datetime'
+            //,tickInterval: 86400000
+        },
+        series: []
+    });
+
+    //sorta global variables...
+    window.chart = chart = $('#container').highcharts(); //
+    window.forwardLevels = [];
+    window.timeSteps = [];
+    window.startingDateForwardCurve;
+    var timeStep = 0; //hourly, daily, monthly, quarterly, yearly... 
+
+    $.getJSON('/Prices/ForwardCurve?region=nordic&nonOverlapping=true', function (data) {
+        startingDateForwardCurve = Date.parse(data[0].Begin);
+
+        for (var i = 0; i < data.length; i++) {
+            var fwd = data[i];
+
+            var price = fwd.FixPrice;
+            var begin = Date.parse(fwd.Begin);
+            var end = Date.parse(fwd.End);
+            //this is daily, timeStep = 86400000
+            var diff = Math.ceil((end - begin) / 86400000);
+
+            var priceData = [];
+            forwardLevels.push(price);
+            timeSteps.push(diff + 1);
+
+            //we must replicate the price daily, so the server won't have that high of a burden
+            for (var j = 0; j <= diff; j++) {
+                priceData[j] = [begin + j * 86400000, price];
+            }
+
+            var serie = {
+                name: fwd.Contract,
+                data: priceData
+            };
+
+            chart.addSeries(serie);
+
+            chart.xAxis[0].setExtremes(priceData[0][0], priceData[priceData.length - 1][0]);
+        }
+        $("#container").trigger("ForwardContractsReady");
+    });
+
     $scope.simulate = function() {
         var data = {
             timeStepsInLevels: timeSteps,
@@ -68,7 +126,7 @@
         };
 
         function postSuccess(confidences) {
-            clearSeriesContainingName("Confidence");
+            clearSeriesContainingName(chart, "Confidence");
             for (var i = 0; i < confidences.length; i++) {
                 var confidenceSerie = confidences[i];
                 var confidenceValues = [];
